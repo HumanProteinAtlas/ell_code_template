69c2537a71f6cc84eb8e23695327605752cb3809
Fredric Johansson <fredric@fredricj.se>
2026-07-05 19:41:17 +0200

    Disable low contrast check when saving
    The output images will always be low contrast

--- examples/cellpose_segmentation/cellpose_segmentation.py
+++ examples/cellpose_segmentation/cellpose_segmentation.py
@@ -88,7 +88,7 @@ def segment(model_nuc, model_cyto, nuclei_img, cyto_img1, cyto_img2, nuc_diamete
         channels=channels,
         diameter=nuc_diameter,
     )
-    imsave(output_folder + "/" + output_prefix + "nuclei_mask.png", nuclei_masks)
+    imsave(output_folder + "/" + output_prefix + "nuclei_mask.png", nuclei_masks, check_contrast=False)
 
     if cyto_img1 is not None:
         cell_masks = None
@@ -110,7 +110,7 @@ def segment(model_nuc, model_cyto, nuclei_img, cyto_img1, cyto_img2, nuc_diamete
                 cellprob_threshold=-0.4,
                 normalize=False,
             )
-            imsave(output_folder + "/" + output_prefix + "sam_raw_mask.png", sam_masks)
+            imsave(output_folder + "/" + output_prefix + "sam_raw_mask.png", sam_masks, check_contrast=False)
 
             cell_masks = merge_segmentations(nuclei_masks, sam_masks, None, nuc_diameter)
 
@@ -123,7 +123,7 @@ def segment(model_nuc, model_cyto, nuclei_img, cyto_img1, cyto_img2, nuc_diamete
                 flow_threshold=0.8,
                 cellprob_threshold=-0.4
             )
-            imsave(output_folder + "/" + output_prefix + "cyto1_mask.png", cyto1_masks)
+            imsave(output_folder + "/" + output_prefix + "cyto1_mask.png", cyto1_masks, check_contrast=False)
 
             if cyto_img2 is not None:
                 cyto2_masks, flows, styles = model_cyto.eval(
@@ -139,4 +139,4 @@ def segment(model_nuc, model_cyto, nuclei_img, cyto_img1, cyto_img2, nuc_diamete
             else:
                 cell_masks = merge_segmentations(nuclei_masks, cyto1_masks, None, nuc_diameter)
 
-        imsave(output_folder + "/" + output_prefix + "cell_mask.png", cell_masks)
+        imsave(output_folder + "/" + output_prefix + "cell_mask.png", cell_masks, check_contrast=False)
